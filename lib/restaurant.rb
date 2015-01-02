7a73e9ae5399ff849a9dad012ec6e86312af8ae0
bibiana <bcristofol@gmail.com>
2015-01-02 17:41:57 +0000

    Restaurant class - added spec 'should not have customers when initialized'

--- lib/restaurant.rb
+++ lib/restaurant.rb
@@ -1,13 +1,17 @@
 require_relative 'menu'
+require_relative 'customer'
 
 class Restaurant
 
   include Menu
 
-  attr_reader :order
+  attr_reader :order, :customers
 
   def initialize
     @order = {}
+    @customers = []
   end
+
+
 
 end
--- spec/restaurant_spec.rb
+++ spec/restaurant_spec.rb
@@ -2,10 +2,23 @@ require 'restaurant'
 
 describe Restaurant do
 	
+  let(:menu) {DishesList.new}
   let(:restaurant) { Restaurant.new }
+  let(:customer) { Customer.new(menu) }
 
   it 'should have an empty order when initialized' do
     expect(restaurant.order).to be_empty
+  end
+
+  it 'should not have customers when initialized' do
+    expect(restaurant.customers).to be_empty
+  end
+
+  xit 'should be able to receive an order from the customer' do
+    dishes = { Tortilla: 2 }
+    total_price = 8
+    customer.add!(dishes, total_price)
+    expect(restaurant.order).not_to be_empty
   end
 
 end
