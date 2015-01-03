2542893d0ae17b4146c222300136b85530547bc5
bibiana <bcristofol@gmail.com>
2015-01-03 12:01:00 +0000

    Restaurant class - added context

--- spec/restaurant_spec.rb
+++ spec/restaurant_spec.rb
@@ -2,33 +2,41 @@ require 'restaurant'
 
 describe Restaurant do
 	
-  let(:menu) {DishesList.new}
+  let(:menu) { DishesList.new }
   let(:restaurant) { Restaurant.new(menu) }
   let(:customer) { Customer.new(menu) }
 
-  it 'should have a menu' do
-    expect(restaurant.menu.dishes_list).to eq(menu.dishes_list)
+  context 'when initialized' do
+
+    it 'should have a menu' do
+      expect(restaurant.menu.dishes_list).to eq(menu.dishes_list)
+    end
+
+    it 'should not have orders' do
+      expect(restaurant).not_to have_orders
+    end
+
+    it 'should not have customers' do
+      expect(restaurant.customers).to be_empty
+    end
+
   end
 
-  it 'should not have orders when initialized' do
-    expect(restaurant).not_to have_orders
-  end
+  context 'Receiving customers and orders' do
 
-  it 'should not have customers when initialized' do
-    expect(restaurant.customers).to be_empty
-  end
+    it 'should be able to have customers' do
+      restaurant.receive!(customer)
+      expect(restaurant.customers).not_to be_empty
+    end
 
-  it 'should be able to have customers' do
-    restaurant.receive!(customer)
-    expect(restaurant.customers).not_to be_empty
-  end
+    it 'should be able to receive an order from the customer' do
+      dishes = { Tortilla: 2 }
+      total_price = 8
+      restaurant.receive!(customer)
+      customer.add!(dishes, total_price)
+      expect(restaurant).to have_orders
+    end
 
-  it 'should be able to receive an order from the customer' do
-    dishes = { Tortilla: 2 }
-    total_price = 8
-    restaurant.receive!(customer)
-    customer.add!(dishes, total_price)
-    expect(restaurant).to have_orders
   end
 
 end
