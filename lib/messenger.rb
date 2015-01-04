3e2f3021b7fb4d10147b2c75ba95260c672b3e3c
bibiana <bcristofol@gmail.com>
2015-01-04 16:48:44 +0000

    Messenger class - added should load from/to phone numbers

--- lib/messenger.rb
+++ lib/messenger.rb
@@ -2,12 +2,14 @@ require 'yaml'
 
 class Messenger
 
-  attr_reader :account_sid, :auth_token
+  attr_reader :account_sid, :auth_token, :sender_num, :receiver_num
 
   def initialize(filename = 'twilio.yaml')
     twilio = File.open( filename ) { |yf| YAML::load( yf ) }
     @account_sid = twilio['sid']
     @auth_token = twilio['token']
+    @sender_num = twilio['from']
+    @receiver_num = twilio['to']
   end
 
 end
--- spec/messenger_spec.rb
+++ spec/messenger_spec.rb
@@ -7,7 +7,8 @@ describe Messenger do
   it 'should load the configuration file' do
     expect(messenger.account_sid).to eq(123)
     expect(messenger.auth_token).to eq(456)
+    expect(messenger.sender_num).to eq("+441")
+    expect(messenger.receiver_num).to eq("+447")
   end
-
 
 end
